2ce28ab55a1aff2071182d7a35da25da5ba39af7
NotNone <notnone.homeryland@gmail.com>
2026-03-14 23:37:10 +0800

    Remove frontend directory and update mise tools

--- frontend/src/pages/index.rs
+++
@@ -1,8 +0,0 @@
-use leptos::prelude::*;
-
-#[component]
-pub fn IndexPage() -> impl IntoView {
-    view! {
-        <h1>Hello index!</h1>
-    }
-}
--- frontend/src/pages/register.rs
+++
@@ -1,8 +0,0 @@
-use leptos::prelude::*;
-
-#[component]
-pub fn RegisterPage() -> impl IntoView {
-    view! {
-        <h1>Hi register</h1>
-    }
-}
