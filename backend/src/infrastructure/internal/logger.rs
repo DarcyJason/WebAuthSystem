5a7ba9b008a3852545ad65cef2dc34c400f9a607
NotNOne <notnone@homeryland.com>
2026-04-26 18:36:44 +0800

    feat: "fix a bug in logger.rs"

--- backend/src/infrastructure/internal/logger.rs
+++ backend/src/infrastructure/internal/logger.rs
@@ -4,7 +4,7 @@ use tracing_subscriber::util::SubscriberInitExt;
 use tracing_subscriber::{EnvFilter, fmt};
 
 pub fn init_logger() -> WorkerGuard {
-    let file_appender = tracing_appender::rolling::daily("../../../logs", "backend.log");
+    let file_appender = tracing_appender::rolling::daily("logs", "backend.log");
     let (non_blocking, guard) = tracing_appender::non_blocking(file_appender);
     let file_layer = fmt::layer().with_writer(non_blocking).with_ansi(false);
     let console_layer = tracing_subscriber::fmt::layer();
